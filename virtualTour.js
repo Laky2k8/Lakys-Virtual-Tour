de953e12128665faa31625b7ed981c9ea92e9e02
Laky2K8 <59142446+Laky2k8@users.noreply.github.com>
2025-12-02 09:43:38 +0000

    first commit

---
+++ virtualTour.js
@@ -0,0 +1,67 @@
+// LAKY'S VIRTUAL TOUR v1.0.0
+
+/* Map Types */
+let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+
+
+const satelliteBase = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+    attribution: 'Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+const labels = L.esri.basemapLayer('ImageryLabels');
+const transportation = L.esri.basemapLayer('ImageryTransportation');
+
+const satelliteMap = L.layerGroup([satelliteBase, transportation, labels ]);
+
+let baseMaps = {
+    "Default": osm,
+    "Satellite": satelliteMap
+};
+
+var map = L.map('map',
+    {
+        minZoom: 6,
+        maxZoom: 19,
+        layers: [osm]
+    }
+);
+map.setView([28.412631731045128, -16.544176864035325], 13);
+
+let layer_control = L.control.layers(baseMaps).addTo(map);
+
+loadLocations().then(
+  function(locations)
+  {
+    placeLocations(locations);
+  }  
+);
+
+function placeLocations(locations)
+{
+    locations.forEach(location => {
+        var marker = L.marker([location.lat, location.lon]);
+        marker.addTo(map);
+    });
+}
+
+async function loadLocations()
+{
+    const url = "locations.json";
+
+    try
+    {
+        const response = await fetch(url);
+        let responseJSON = await response.json();
+
+        console.log(responseJSON);
+        return responseJSON;
+    }
+    catch (error)
+    {
+        console.error(error.message);
+    }
+}
